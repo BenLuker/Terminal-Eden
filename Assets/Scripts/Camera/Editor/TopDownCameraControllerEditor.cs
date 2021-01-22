e7b3b997d3daa0740b1ba3b2a342ebcee53574c9
BenLuker <thebenluker@gmail.com>
2021-01-22 13:19:09 -0800

    Added Top Down Camera Controller

---
+++ Assets/Scripts/Camera/Editor/TopDownCameraControllerEditor.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TopDownCameraController))]
+public class TopDownCameraControllerEditor : Editor
+{
+
+    TopDownCameraController _cam;
+
+    void OnEnable()
+    {
+        _cam = (TopDownCameraController)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        // Monobehavior Header
+        GUI.enabled = false;
+        EditorGUILayout.ObjectField("Script:", MonoScript.FromMonoBehaviour((TopDownCameraController)target), typeof(TopDownCameraController), false);
+        EditorGUILayout.Space();
+
+        // Transforms
+        EditorGUILayout.ObjectField("Camera Transform", _cam.cameraTransform, typeof(Transform), true);
+        EditorGUILayout.ObjectField("Crane Transform", _cam.craneTransform, typeof(Transform), true);
+        EditorGUILayout.Space();
+        GUI.enabled = true;
+
+        // Create styles
+        GUIStyle headerStyle = new GUIStyle();
+        headerStyle.fontStyle = FontStyle.Bold;
+
+        // Smoothing slider
+        _cam.cameraSmoothingSpeed = EditorGUILayout.Slider("Camera Smoothing Speed", _cam.cameraSmoothingSpeed, 0.1f, 20f);
+        EditorGUILayout.Space();
+
+        // Camera Limits (Angle and Zoom)
+        GUILayout.Label("Camera Limits", headerStyle);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Camera Angle", GUILayout.Width(125));
+        _cam.craneAngleMin = EditorGUILayout.FloatField(_cam.craneAngleMin, GUILayout.Width(50));
+        EditorGUILayout.MinMaxSlider(ref _cam.craneAngleMin, ref _cam.craneAngleMax, 0, 90);
+        _cam.craneAngleMax = EditorGUILayout.FloatField(_cam.craneAngleMax, GUILayout.Width(50));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Camera Distance", GUILayout.Width(125));
+        _cam.cameraDistanceMin = EditorGUILayout.FloatField(_cam.cameraDistanceMin, GUILayout.Width(50));
+        EditorGUILayout.MinMaxSlider(ref _cam.cameraDistanceMin, ref _cam.cameraDistanceMax, 0, 200);
+        _cam.cameraDistanceMax = EditorGUILayout.FloatField(_cam.cameraDistanceMax, GUILayout.Width(50));
+        GUILayout.EndHorizontal();
+
+        EditorGUILayout.Space();
+
+        // Mouse Inputs
+        GUILayout.Label("Mouse Input", headerStyle);
+        _cam.mouseRotationSpeed = EditorGUILayout.Slider("Rotation Speed", _cam.mouseRotationSpeed, 0.05f, 0.5f);
+        _cam.mouseZoomSpeed = EditorGUILayout.Slider("Zoom Speed", _cam.mouseZoomSpeed, 0.1f, 10f);
+        EditorGUILayout.Space();
+
+        // Keyboard Inputs
+        GUILayout.Label("Keyboard Input", headerStyle);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Movement Speed", GUILayout.Width(125));
+        _cam.keyboardMovementSpeedSlow = EditorGUILayout.FloatField(_cam.keyboardMovementSpeedSlow, GUILayout.Width(50));
+        EditorGUILayout.MinMaxSlider(ref _cam.keyboardMovementSpeedSlow, ref _cam.keyboardMovementSpeedFast, 0.01f, 0.5f);
+        _cam.keyboardMovementSpeedFast = EditorGUILayout.FloatField(_cam.keyboardMovementSpeedFast, GUILayout.Width(50));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Rotation Speed", GUILayout.Width(125));
+        _cam.keyboardRotationSpeedSlow = EditorGUILayout.FloatField(_cam.keyboardRotationSpeedSlow, GUILayout.Width(50));
+        EditorGUILayout.MinMaxSlider(ref _cam.keyboardRotationSpeedSlow, ref _cam.keyboardRotationSpeedFast, 0.1f, 1f);
+        _cam.keyboardRotationSpeedFast = EditorGUILayout.FloatField(_cam.keyboardRotationSpeedFast, GUILayout.Width(50));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Zoom Speed", GUILayout.Width(125));
+        _cam.keyboardZoomSpeedSlow = EditorGUILayout.FloatField(_cam.keyboardZoomSpeedSlow, GUILayout.Width(50));
+        EditorGUILayout.MinMaxSlider(ref _cam.keyboardZoomSpeedSlow, ref _cam.keyboardZoomSpeedFast, 0.01f, 5f);
+        _cam.keyboardZoomSpeedFast = EditorGUILayout.FloatField(_cam.keyboardZoomSpeedFast, GUILayout.Width(50));
+        GUILayout.EndHorizontal();
+    }
+}
---
+++ Assets/Scripts/Camera/TopDownCameraController.cs
@@ -0,0 +1,212 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCameraController : MonoBehaviour
+{
+
+    public Transform cameraTransform;
+    public Transform craneTransform;
+    public float cameraSmoothingSpeed = 10f;
+
+    [Header("Keyboard Input")]
+    public float keyboardMovementSpeedSlow = 0.04f;
+    public float keyboardMovementSpeedFast = 0.1f;
+    public float keyboardRotationSpeedSlow = 0.3f;
+    public float keyboardRotationSpeedFast = 0.3f;
+    public float keyboardZoomSpeedSlow = 5f;
+    public float keyboardZoomSpeedFast = 5f;
+
+    float keyboardMovementSpeed;
+    float keyboardRotationSpeed;
+    float keyboardZoomSpeed;
+
+    [Header("Mouse Input")]
+    public float mouseRotationSpeed = 0.2f;
+    public float mouseZoomSpeed = 2f;
+
+    [Header("Camera Limits")]
+    public float craneAngleMin = 15f;
+    public float craneAngleMax = 60f;
+    public float cameraDistanceMin = 50f;
+    public float cameraDistanceMax = 100f;
+
+    // Desired position, rotation, and zoom
+    Vector3 newPos;
+    Quaternion newRotX;
+    Quaternion newRotY;
+    Vector3 newZoom;
+
+    // Mouse Positions
+    Vector3 dragStartPosition;
+    Vector3 dragCurrentPosition;
+    Vector3 rotateStartPosition;
+    Vector3 rotateCurrentPosition;
+
+    #region Events
+
+    void Start()
+    {
+        craneTransform = transform.GetChild(0);
+        cameraTransform = craneTransform.GetChild(0);
+        newPos = transform.position;
+        newRotX = transform.rotation;
+        newRotY = craneTransform.localRotation;
+        newZoom = cameraTransform.localPosition;
+    }
+
+    void Update()
+    {
+        HandleMouseInput();
+        HandleKeyboardInput();
+    }
+
+    void FixedUpdate()
+    {
+        // Clamp to limits
+        if (newRotY.eulerAngles.x < craneAngleMin)
+        {
+            newRotY = Quaternion.Euler(Vector3.right * craneAngleMin);
+        }
+        if (newRotY.eulerAngles.x > craneAngleMax)
+        {
+            newRotY = Quaternion.Euler(Vector3.right * craneAngleMax);
+        }
+        if (newZoom.z > -cameraDistanceMin)
+        {
+            newZoom = Vector3.forward * -cameraDistanceMin;
+        }
+        if (newZoom.z < -cameraDistanceMax)
+        {
+            newZoom = Vector3.forward * -cameraDistanceMax;
+        }
+
+        // Lerp transformations
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * cameraSmoothingSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotX, Time.deltaTime * cameraSmoothingSpeed);
+        craneTransform.localRotation = Quaternion.Lerp(craneTransform.localRotation, newRotY, Time.deltaTime * cameraSmoothingSpeed);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * cameraSmoothingSpeed);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(cameraTransform.position, transform.position);
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(newPos, transform.position);
+        Gizmos.DrawSphere(newPos, 0.075f);
+
+        Gizmos.color = Color.gray;
+        Gizmos.DrawSphere(transform.position, 0.15f);
+    }
+
+    #endregion
+
+    void HandleMouseInput()
+    {
+        // Translate
+        if (Input.GetMouseButtonDown(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragStartPosition = ray.GetPoint(entry);
+            }
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragCurrentPosition = ray.GetPoint(entry);
+
+                newPos = transform.position + dragStartPosition - dragCurrentPosition;
+            }
+        }
+
+        // Rotate
+        if (Input.GetMouseButtonDown(1))
+        {
+            rotateStartPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            rotateCurrentPosition = Input.mousePosition;
+            Vector3 difference = rotateCurrentPosition - rotateStartPosition;
+
+            newRotX *= Quaternion.Euler(Vector3.up * (difference.x * mouseRotationSpeed));
+            newRotY *= Quaternion.Euler(Vector3.right * (difference.y * -mouseRotationSpeed));
+
+            rotateStartPosition = rotateCurrentPosition;
+        }
+
+        // Zoom
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            newZoom += Vector3.forward * (Input.mouseScrollDelta.y * mouseZoomSpeed);
+        }
+    }
+
+    void HandleKeyboardInput()
+    {
+        // Speed up keyboard controls if holding shift
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            keyboardMovementSpeed = keyboardMovementSpeedFast;
+            keyboardRotationSpeed = keyboardRotationSpeedFast;
+            keyboardZoomSpeed = keyboardZoomSpeedFast;
+        }
+        else
+        {
+            keyboardMovementSpeed = keyboardMovementSpeedSlow;
+            keyboardRotationSpeed = keyboardRotationSpeedSlow;
+            keyboardZoomSpeed = keyboardZoomSpeedSlow;
+        }
+
+        // Translate
+        Vector2 keyboardMove = Vector2.zero;
+        keyboardMove.y += Input.GetKey(KeyCode.W) ? 1 : 0;
+        keyboardMove.y -= Input.GetKey(KeyCode.S) ? 1 : 0;
+        keyboardMove.x += Input.GetKey(KeyCode.D) ? 1 : 0;
+        keyboardMove.x -= Input.GetKey(KeyCode.A) ? 1 : 0;
+        keyboardMove.Normalize();
+
+        newPos += (transform.forward * keyboardMove.y * keyboardMovementSpeed);
+        newPos += (transform.right * keyboardMove.x * keyboardMovementSpeed);
+
+        // Rotate
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            newRotX *= Quaternion.Euler(Vector3.up * keyboardRotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            newRotX *= Quaternion.Euler(Vector3.up * -keyboardRotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            newRotY *= Quaternion.Euler(Vector3.right * keyboardRotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            newRotY *= Quaternion.Euler(Vector3.right * -keyboardRotationSpeed);
+        }
+
+        // Zoom
+        if (Input.GetKey(KeyCode.E))
+        {
+            newZoom += (Vector3.forward * keyboardZoomSpeed);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            newZoom += (Vector3.forward * -keyboardZoomSpeed);
+        }
+    }
+}
